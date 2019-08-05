08551e8cc9ed3bb8ed9e62baa2fa80c91b268326
alex <win32coder@list.ru>
2019-08-05 10:54:39 +0300

    Add project files and models

---
+++ PhoneApp/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneApp.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Phone> Phones { get; set; }
+        public Company()
+        {
+            Phones = new List<Phone>();
+        }
+    }
+}
