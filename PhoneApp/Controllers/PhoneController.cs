80b59add2a4f5ab6f6f3235861c1fbb34f5b8e28
alex <win32coder@list.ru>
2019-08-05 12:59:27 +0300

    Added nLog logging

--- PhoneApp/Controllers/PhoneController.cs
+++ PhoneApp/Controllers/PhoneController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PhoneApp.Models;
 
 namespace PhoneApp.Controllers
@@ -10,15 +11,18 @@ namespace PhoneApp.Controllers
     public class PhoneController : Controller
     {
         private readonly AppDbContext dbContext;
+        private readonly ILogger<PhoneController> logger;
 
-        public PhoneController(AppDbContext dbContext)
+        public PhoneController(AppDbContext dbContext, ILogger<PhoneController> logger)
         {
             this.dbContext = dbContext;
+            this.logger = logger;
         }
 
         public IActionResult Index()
         {
             var list = dbContext.Phones.ToList();
+            logger.LogInformation($"PhoneController.Index method called!");
             return View(list);
         }
     }
--- PhoneApp/Program.cs
+++ PhoneApp/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using PhoneApp.Models;
 
 namespace PhoneApp
@@ -16,31 +17,39 @@ namespace PhoneApp
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
             {
-                var services = scope.ServiceProvider;
-
-                try
+                var host = CreateWebHostBuilder(args).Build();
+                using (var scope = host.Services.CreateScope())
                 {
-                    var dbContext = services.GetRequiredService<AppDbContext>();
-                    SampleData.Initialize(dbContext);
+                    var services = scope.ServiceProvider;
+                        var dbContext = services.GetRequiredService<AppDbContext>();
+                        SampleData.Initialize(dbContext);
                 }
-                catch (Exception ex)
-                {
-
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-
-            }    
-                
-                
                 host.Run();
+            }
+            catch (Exception e)
+            {
+
+                logger.Error(e, "An error occurred while seeding the database.");
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Information);
+            })
+            .UseNLog();
     }
 }
