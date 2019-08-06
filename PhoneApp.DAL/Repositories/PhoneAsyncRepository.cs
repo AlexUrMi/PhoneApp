908cb903cf1c9c49a63d05898fad0d96f02c58aa
alex <win32coder@list.ru>
2019-08-06 21:20:26 +0300

    Fixed minor issue

---
+++ PhoneApp.DAL/Repositories/PhoneAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneApp.Models
+{
+    public interface IPhoneAsyncRepository : IGenericAsyncRepository<Phone>
+    {
+
+    }
+    public class PhoneAsyncRepository : GenericAsyncRepository<Phone>, IPhoneAsyncRepository
+    {
+        public PhoneAsyncRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
--- PhoneApp.DAL/Repositories/PhoneRepository.cs
+++ PhoneApp.DAL/Repositories/PhoneRepository.cs
@@ -12,15 +12,5 @@
         }
     }
 
-    public interface IPhoneAsyncRepository : IGenericAsyncRepository<Phone>
-    {
 
-    }
-
-    public class PhoneAsyncRepository : GenericAsyncRepository<Phone>, IPhoneAsyncRepository
-    {
-        public PhoneAsyncRepository(AppDbContext context) : base(context)
-        {
-        }
-    }
 }
